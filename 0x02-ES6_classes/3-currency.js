a077fb9a55560d688b98c2681c96185321d73ea9
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-06-28 20:55:19 +0300

    Fix code

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -2,57 +2,57 @@
  * Represents a currency.
  */
 export default class Currency {
-    /**
+  /**
      * Creates a new Currency instance.
      * @param {string} code - The currency code.
      * @param {string} name - The currency name.
      * @throws {Error} If code or name is not a string.
      */
-    constructor(code, name) {
-      if (typeof code !== 'string' || typeof name !== 'string') {
-        throw new Error('Code and name must be strings');
-      }
-      this.code = code;
-      this.name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') {
+      throw new Error('Code and name must be strings');
     }
-  
-    /**
+    this.code = code;
+    this.name = name;
+  }
+
+  /**
      * Gets the currency code.
      * @returns {string} The currency code.
      */
-    get code() {
-      return this._code;
-    }
-  
-    /**
+  get code() {
+    return this._code;
+  }
+
+  /**
      * Sets the currency code.
      * @param {string} val - The new currency code.
      */
-    set code(val) {
-      this._code = val;
-    }
-  
-    /**
+  set code(val) {
+    this._code = val;
+  }
+
+  /**
      * Gets the currency name.
      * @returns {string} The currency name.
      */
-    get name() {
-      return this._name;
-    }
-  
-    /**
+  get name() {
+    return this._name;
+  }
+
+  /**
      * Sets the currency name.
      * @param {string} val - The new currency name.
      */
-    set name(val) {
-      this._name = val;
-    }
-  
-    /**
+  set name(val) {
+    this._name = val;
+  }
+
+  /**
      * Displays the full currency format.
      * @returns {string} The full currency format.
      */
-    displayFullCurrency() {
-      return `${this.name} (${this.code})`;
-    }
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
   }
+}
