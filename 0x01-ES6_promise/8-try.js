7e878a253daeaba8cf023be5d0b5e367cf8622e5
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-02 18:19:27 +0300

    Fix Checker errors

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,4 @@
-import divideFunction from './8-try';
-
-console.log(divideFunction(10, 2));
-console.log(divideFunction(10, 0));
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  return numerator / denominator;
+}
