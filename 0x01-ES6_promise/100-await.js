1758c8cd0e3a4fb1886a8cbf609b6a1736fbb8d8
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-02 18:16:54 +0300

    Add file

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,18 @@
-import { uploadPhoto, createUser } from '../utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    return { photo, user };
-  } catch (error) {
-    return { photo: null, user: null };
+    const resFromUploadPhoto = await uploadPhoto();
+    const resFromCreateUser = await createUser();
+    return {
+      photo: resFromUploadPhoto,
+      user: resFromCreateUser,
+    };
+  } catch
+  (err) {
+    return {
+      photo: null,
+      user: null,
+    };
   }
 }
