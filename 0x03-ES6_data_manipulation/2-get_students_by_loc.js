b1111b488f99878844bb8b07df007af149e634ef
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-07-02 23:27:55 +0300

    Fix lint errors

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,13 +1,5 @@
 import getListStudents from './0-get_list_students';
 
-/**
- * Filters a list of students by their location.
- *
- * @param {string} city - The city to filter students by.
- * @param {Array} [student=getListStudents()] - The list of students to filter.
- * @returns {Array} - The filtered list of students located in the specified city.
- */
-export default function getStudentsByLocation(city, student = getListStudents()) {
-  // Use the filter method to create a new array with students located in the specified city
+export default function getStudentsByLocation(student = getListStudents(), city) {
   return student.filter((el) => el.location === city);
 }
