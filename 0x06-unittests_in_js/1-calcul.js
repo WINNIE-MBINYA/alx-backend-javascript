176f9139bd541f50afa4fb3af3c2883148edac05
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-08-23 00:49:39 +0300

    Add task 2 files

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,15 @@
+function calculateNumber(type, a, b) {
+    const numA = Math.round(a);
+    const numB = Math.round(b);
+
+    if (type === 'SUM') {
+        return numA + numB;
+    } else if (type === 'SUBTRACT') {
+        return numA - numB;
+    } else if (type === 'DIVIDE') {
+        if (numB === 0) return 'Error';
+        return numA / numB;
+    }
+}
+
+module.exports = calculateNumber;
